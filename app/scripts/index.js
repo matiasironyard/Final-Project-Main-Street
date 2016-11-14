f22d6061a60d21593c87a0f396c714d2084cdb4a
Matias Mariani <matiasironyard@gmail.com>
2016-11-14 17:17:29 -0500

    update

---
+++ app/scripts/index.js
@@ -0,0 +1,18 @@
+console.log("Hello World!");
+var $ = require('jquery');
+var Backbone = require('backbone');
+
+
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://api.yelp.com/v3/businesses/search/phone?phone=%2018645955240",
+  "method": "GET",
+  "headers": {
+    "authorization": "Bearer i565rg5Yi8NoRyCmfTHX5ngjnlRSCmkQkG0CHSibDWzBY2OJQd3LNU0WfEsf9XibRZAiCBriAohxLkjbsZ4IfqMi-al9M4xp9DYM9vGZM6MlyNW45Z8earlIU94pWHYx",
+  }
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
